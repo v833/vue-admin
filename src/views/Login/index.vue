70575c29235470a2eb14e8982766125641b40841
v833 <2507301541@qq.com>
2021-06-25 00:27:20 +0800

    表单初步验证

--- src/views/Login/index.vue
+++ src/views/Login/index.vue
@@ -8,6 +8,36 @@
             @click="toggleMenu(item)"
             >{{ item.txt }}</li>
       </ul>
+      <!-- 表单验证 -->
+      <el-form :model="ruleForm"
+               status-icon
+               :rules="rules"
+               ref="ruleForm"
+               class="login-form"
+               size="medium">
+        <el-form-item prop="username" class="item-form">
+          <label for="">用户名</label>
+          <el-input type="text" v-model="ruleForm.username" autocomplete="off"></el-input>
+        </el-form-item>
+        <el-form-item prop="password" class="item-form">
+          <label for="">密码</label>
+          <el-input type="password" v-model="ruleForm.password" autocomplete="off" minlength="6" maxlength="20"></el-input>
+        </el-form-item>
+        <el-form-item prop="code">
+          <label for="">验证码</label>
+          <el-row :gutter="11">
+            <el-col :span="15">
+              <el-input v-model="ruleForm.code" minlangth="6" maxlength="6"></el-input>
+            </el-col>
+            <el-col :span="9">
+              <el-button type="success" class="block">获取验证码</el-button>
+            </el-col>
+          </el-row>
+        </el-form-item>
+        <el-form-item>
+          <el-button type="danger" class="block" @click="submitForm('ruleForm')">提交</el-button>
+        </el-form-item>
+      </el-form>
     </div>
   </div>
 </template>
@@ -16,20 +46,76 @@
 export default {
   name: 'login',
   data() {
+    var validateCode = (rule, value, callback) => {
+      let reg = /^[a-z0-9]{6}$/
+      if (value === '') {
+        callback(new Error('请输入验证码'));
+      } else if (!reg.test(value)) {
+        callback(new Error('验证码格式错误'));
+      } else {
+        callback()
+      }
+    };
+    var validateUsername = (rule, value, callback) => {
+      let reg = /^([a-zA-Z0-9]+[_|_|\-|.]?)*[a-zA-Z0-9]+@([a-zA-Z0-9]+[_|_|.]?)*[a-zA-Z0-9]+\.[a-zA-Z]{2,3}$/
+      if (value === '') {
+        callback(new Error('请输入用户名'));
+      } else if (!reg.test(value)) {
+        callback(new Error('用户名格式错误'));
+      } else {
+        callback()
+      }
+    };
+    var validatePassword = (rule, value, callback) => {
+      let reg = /^(\w){6,20}$/;
+      if (value === '') {
+        callback(new Error('请输入密码'));
+      } else if (!reg.test(value)) {
+        callback(new Error('密码格式错误'));
+      } else {
+        callback();
+      }
+    };
     return {
       menuTab: [
         {txt: '登录', current: true},
         {txt: '注册', current: false}
-      ]
+      ],
+      ruleForm: {
+        username: '',
+        password: '',
+        code: ''
+      },
+      rules: {
+        username: [
+          { validator: validateUsername, trigger: 'blur' }
+        ],
+        password: [
+          { validator: validatePassword, trigger: 'blur' }
+        ],
+        code: [
+          { validator: validateCode, trigger: 'blur' }
+        ]
+      }
     }
   },
   methods: {
     toggleMenu(data) {
       this.menuTab.forEach(item => item.current = false)
       data.current = true
+    },
+    submitForm(formName) {
+        this.$refs[formName].validate((valid) => {
+          if (valid) {
+            alert('submit!');
+          } else {
+            console.log('error submit!!');
+            return false;
+          }
+        });
+      },
     }
   }
-}
 </script>
 
 <style lang="scss" scoped>
@@ -39,6 +125,7 @@ export default {
     .login-wrap {
       width: 330px;
       margin: auto;
+      padding: 100px;
       .menu-tab {
         text-align: center;
         li {
@@ -52,6 +139,18 @@ export default {
         }
         .current {
           background-color: rgba(0,0,0,.1);
+        }
+      }
+      .login-form {
+        margin-top: 29px;
+        label {
+          display: block;
+          font-size: 14px;
+          color: white;
+        }
+        .block {
+          width: 100%;
+          display: block;
         }
       }
     }
